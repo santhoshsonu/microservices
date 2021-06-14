e6bcb7c5adb54b519235e7a867240e332f490e6a
santhoshsonu <santhosheshwaramma@gmail.com>
2021-06-14 22:17:37 +0530

    updated services urls

--- blog/client/src/config/config.js
+++ blog/client/src/config/config.js
@@ -2,18 +2,18 @@ const config = {
     PORT: process.env.PORT,
     SERVICES: {
         POSTS: {
-            BASE_URL: 'http://localhost:3000',
+            BASE_URL: 'http://microservices.blog.com',
             LIST: '/posts',
             ADD: '/posts',
             GET_POST: '/posts/:pid'
         },
         COMMENTS: {
-            BASE_URL: 'http://localhost:3001',
+            BASE_URL: 'http://microservices.blog.com',
             LIST: '/posts/:pid/comments',
             ADD: '/posts/:pid/comments'
         },
         QUERY: {
-            BASE_URL: 'http://localhost:3002',
+            BASE_URL: 'http://microservices.blog.com',
             LIST: '/query/posts'
         }
     }
