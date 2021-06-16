06386dd0d6240e0e334a2492f6172cf631ede8b5
santhoshsonu <santhosheshwaramma@gmail.com>
2021-06-16 21:16:47 +0530

    Refacatored

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { authRouter } from './routes/auth';
 
 const port: number = 3000;
 const app = express();
 app.use(json());
 
+app.use('/api/users/', authRouter);
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
