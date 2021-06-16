49b07c1ccaf7d86692aafaf9e13bc079a4166478
santhoshsonu <santhosheshwaramma@gmail.com>
2021-06-16 21:58:39 +0530

    Added Global Error handler

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,12 +1,33 @@
 import express from 'express';
 import { json } from 'body-parser';
+
+import { errorHandler } from './middlewares/error-handler';
 import { authRouter } from './routes/auth';
 
+/**
+ * App port
+ */
 const port: number = 3000;
+
 const app = express();
 app.use(json());
 
+/**
+ * App routes
+ */
 app.use('/api/users/', authRouter);
+
+/**
+ * catch 404 errors and pass to global error hanlder
+ */
+app.use((_req: express.Request, _res: express.Response, next: express.NextFunction) => {
+  return next(new Error());
+});
+
+/**
+ * Global Error handler middleware
+ */
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
