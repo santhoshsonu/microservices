1338c3e0eba77a1f5f062158486a704534ec9a75
santhoshsonu <santhosheshwaramma@gmail.com>
2021-06-17 18:51:23 +0530

    modified to handle custom errors

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -1,10 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 
+import { CustomError } from '../utils/errors/custom-error';
+
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(400).json({ msg: 'Something went wrong' });
+  if (err instanceof CustomError)
+    return res.status(err.statusCode).json({ errors: err.serializeErrors() });
+  return res.status(500).json({ errors: [{ message: 'Oops! Something went wrong' }] });
 };
