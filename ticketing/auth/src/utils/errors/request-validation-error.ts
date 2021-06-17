b74ce59cbd245e0855e16f7690aaa719434d735e
santhoshsonu <santhosheshwaramma@gmail.com>
2021-06-17 18:54:36 +0530

    Added api request validation errors structure

---
+++ ticketing/auth/src/utils/errors/request-validation-error.ts
@@ -0,0 +1,17 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+  statusCode = 400;
+  constructor(private errors: ValidationError[]) {
+    super('Bad Request');
+    this.errors = errors;
+
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+
+  serializeErrors() {
+    return this.errors.map(e => ({ message: e.msg, field: e.param }));
+  }
+}
