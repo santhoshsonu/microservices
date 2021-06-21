d325bb5a251ea2aa63b6c1ffe4a0c02083859793
santhoshsonu <santhosheshwaramma@gmail.com>
2021-06-21 08:37:16 +0530

    Middleware to handle validation errors

---
+++ ticketing/auth/src/middlewares/validate-request.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
+import { RequestValidationError } from '../utils/errors/request-validation-error';
+
+export const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new RequestValidationError(errors.array());
+    throw error;
+    // return res.json(error.statusCode).json(error.serializeErrors());
+  }
+  next();
+}
