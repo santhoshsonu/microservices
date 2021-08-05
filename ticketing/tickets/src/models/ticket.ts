3c9b6c4a61ada78cff3aaff368dca7a809e9b235
santhoshsonu <santhosheshwaramma@gmail.com>
2021-08-05 18:38:02 +0530

    Export TicketDoc

--- ticketing/tickets/src/models/ticket.ts
+++ ticketing/tickets/src/models/ticket.ts
@@ -50,4 +50,4 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
-export { Ticket };
+export { Ticket, TicketDoc };
