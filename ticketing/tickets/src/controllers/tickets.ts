37c135b1e321f142e9b824cb0d84c9c734bd207b
santhoshsonu <santhosheshwaramma@gmail.com>
2021-08-05 18:37:52 +0530

    Implemented GetTicket By id and Update ticket

--- ticketing/tickets/src/controllers/tickets.ts
+++ ticketing/tickets/src/controllers/tickets.ts
@@ -1,6 +1,6 @@
-import { DatabaseConnectionError } from '@microservice-tickets/common';
+import { DatabaseConnectionError, NotFoundError, UnAuthorizedError } from '@microservice-tickets/common';
 import { NextFunction, Request, Response } from 'express';
-import { Ticket } from '../models/ticket';
+import { Ticket, TicketDoc } from '../models/ticket';
 
 /**
  * Add a new Ticket
@@ -21,4 +21,72 @@ export const createTicket = async (req: Request, res: Response, next: NextFuncti
     return next(new DatabaseConnectionError());
   }
   res.status(201).json(ticket);
+};
+
+/**
+ * Get Ticket by id
+ * Request path params
+ * @param id: string
+ */
+export const getTickets = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const tickets = await Ticket.find({});
+    res.json(tickets);
+  } catch (_) {
+    return next(new DatabaseConnectionError());
+  }
+}
+
+/**
+ * Get Ticket by id
+ * Request path params
+ * @param id: string
+ */
+export const getTicket = async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params?.id;
+  try {
+    const ticket = await Ticket.findById(id);
+    if (!ticket) {
+      return next(new NotFoundError());
+    }
+    res.status(200).json(ticket);
+  } catch (error) {
+    return next(new DatabaseConnectionError());
+  }
+};
+
+/**
+ * Update Ticket
+ * Request path params
+ * @param id: string
+ * Request Body params
+ * @param title: string
+ * @param price: number
+ */
+export const updateTicket = async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params?.id;
+  let ticket: TicketDoc | null;
+  try {
+    ticket = await Ticket.findById(id);
+    if (!ticket) {
+      return next(new NotFoundError());
+    }
+  } catch (error) {
+    return next(new DatabaseConnectionError());
+  }
+
+  if (ticket.userId !== req.currentUser!.id) {
+    return next(new UnAuthorizedError());
+  }
+
+  const { title, price } = req.body;
+  try {
+    ticket.set({
+      title, price
+    });
+    await ticket.save();
+  } catch (error) {
+    return next(new DatabaseConnectionError());
+  }
+  res.status(200).json(ticket);
 };
--- ticketing/tickets/src/routes/tickets.ts
+++ ticketing/tickets/src/routes/tickets.ts
@@ -1,10 +1,13 @@
 import { requireAuth, validateRequest } from '@microservice-tickets/common';
 import express from 'express';
 import { check } from 'express-validator';
-import { createTicket } from '../controllers/tickets';
+import { createTicket, getTicket, getTickets, updateTicket } from '../controllers/tickets';
 
 const router = express.Router();
 
+/**
+ * Add new ticket route
+ */
 router.post('/', requireAuth,
   [
     check('title').notEmpty().withMessage('Title is required'),
@@ -14,5 +17,36 @@ router.post('/', requireAuth,
   ],
   validateRequest,
   createTicket);
+
+/**
+ * List all tickets route
+*/
+router.get('/',
+  getTickets
+);
+
+/**
+ * Get ticket by id route
+*/
+router.get('/:id',
+  requireAuth,
+  getTicket
+);
+
+/**
+ * Update ticket by id route
+*/
+router.put('/:id',
+  requireAuth,
+  [
+    check('title').notEmpty().withMessage('Title is required'),
+    check('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0')
+  ],
+  validateRequest,
+  updateTicket
+);
+
 
 export { router as ticketsRouter };
