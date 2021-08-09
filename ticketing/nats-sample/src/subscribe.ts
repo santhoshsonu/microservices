e0e918c420b30288e21865b34e81bb9c5ea33c43
santhoshsonu <santhosheshwaramma@gmail.com>
2021-08-09 16:21:09 +0530

    Added Queue Group for subscription

--- ticketing/nats-sample/src/subscribe.ts
+++ ticketing/nats-sample/src/subscribe.ts
@@ -10,7 +10,10 @@ const stan = nats.connect('ticketing-nats', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  // The event will be delivered to exactly one 
+  // random member of the Queue Group 
+  // (**Very useful when scaling the service horizontally)
+  const subscription = stan.subscribe('ticket:created', 'orders-service-queue-group');
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
