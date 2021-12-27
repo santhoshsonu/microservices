3ee9532fa007ddbca1df2f0523c34d814aad154c
santhoshsonu <santhosheshwaramma@gmail.com>
2021-12-27 21:44:33 +0530

    Remove unnecessary imports

--- ticketing/tickets/src/events/publishers/ticket-updated-publisher.ts
+++ ticketing/tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -1,4 +1,4 @@
-import { Publisher, Subjects, TicketCreatedEvent, TicketUpdatedEvent } from "@microservice-tickets/common";
+import { Publisher, Subjects, TicketUpdatedEvent } from "@microservice-tickets/common";
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent>{
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
