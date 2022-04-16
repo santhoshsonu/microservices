a86f4a6ade5a10dd6a980b64b3c2e503d88f67b9
santhoshsonu <santhosheshwaramma@gmail.com>
2022-04-17 00:56:01 +0530

    Add redis host config variable

--- ticketing/expiration/src/config/config.ts
+++ ticketing/expiration/src/config/config.ts
@@ -1,9 +1,11 @@
 
 export const config: {
+  REDIS_HOST: string | undefined;
   NATS_URL: string | undefined;
   NATS_CLUSTER_ID: string | undefined;
   NATS_CLIENT_ID: string | undefined;
 } = {
+  REDIS_HOST: process.env.REDIS_HOST,
   NATS_URL: process.env.NATS_URI,
   NATS_CLUSTER_ID: process.env.NATS_CLUSTER_ID,
   NATS_CLIENT_ID: process.env.NATS_CLIENT_ID
--- ticketing/expiration/src/index.ts
+++ ticketing/expiration/src/index.ts
@@ -1,12 +1,15 @@
 import { config } from './config/config';
 import { natsWrapper } from './nats-wrapper';
-
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 /**
  * NATS connection &
  * App startup
  */
 const start = async () => {
+  if (!config.REDIS_HOST) {
+    throw new Error('Env variable: REDIS_HOST must be defined');
+  }
   if (!config.NATS_CLUSTER_ID) {
     throw new Error('Env variable: NATS_CLUSTER_ID must be defined');
   }
@@ -31,6 +34,7 @@ const start = async () => {
     process.on('SIGTERM', () => natsWrapper.client.close());
     process.on('SIGINT', () => natsWrapper.client.close());
 
+    new OrderCreatedListener(natsWrapper.client).listen();
   } catch (err) { console.log(err); };
 
 };
