ad8634bbe5341d0f550466181bde68597ee1e716
santhoshsonu <santhosheshwaramma@gmail.com>
2022-04-17 17:31:16 +0530

    FIX: Populate ticket document

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -11,7 +11,7 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
     queueGroupName: string = QUEQUE_GROUP_NAME;
 
     async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-        const order = await Order.findById(data.orderId);
+        const order = await Order.findById(data.orderId).populate('ticket');
         if (!order) throw new Error("Order not found");
 
         order.set({ status: OrderStatus.Cancelled });
